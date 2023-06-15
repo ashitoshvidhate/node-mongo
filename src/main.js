687daa2c4b2ef9d8336c0afe8f9801e4bf057242
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-15 17:56:12 +0530

    first mongo insert

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log("Ashitosh Vidhate");
+import { MongoClient } from "mongodb";
+
+async function main(){
+    const uri="mongodb://127.0.0.1:27017";
+
+    //open the connection
+    const client=new MongoClient(uri);
+
+    const db=client.db("mydb");
+    const library=db.collection("library");
+
+   // const res=await library.insertOne({});
+    const res=await library.insertOne({"name":"Ashitosh Vidhate"});
+
+    //close the connection
+    await client.close();
+
+    console.log("Record Added");
+
+}
+
+main();
