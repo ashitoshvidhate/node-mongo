f4192f823a0aaaf42ceb4e560a06b22ec1caa548
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-16 01:35:46 +0530

    incomplete

--- src/main.js
+++ src/main.js
@@ -1,26 +1,7 @@
 import { MongoClient } from "mongodb";
 
 //passing parameters to insert function
-async function insertRecord(jsonDocument){
-    const uri="mongodb://127.0.0.1:27017";
 
-    //open the connection
-    const client=new MongoClient(uri);
-
-    const db=client.db("mydb");
-    const library=db.collection("library");
-
-   // const res=await library.insertOne({});
-   //let jsonDocument={message:"Hello world!",to:"Ashitosh" ,from:"Atish"};
-
-   await library.insertOne(jsonDocument);
-
-    //close the connection
-    await client.close();
-
-    console.log("Record Added");
-
-}
 
 async function main(){
     let jsonDocument={message:"Hello world!",to:"Ashitosh" ,from:"Atish"};
---
+++ src/mydb.js
@@ -0,0 +1,22 @@
+async function insertRecord(jsonDocument){
+    const uri="mongodb://127.0.0.1:27017";
+
+    //open the connection
+    const client=new MongoClient(uri);
+
+    const db=client.db("mydb");
+    const library=db.collection("library");
+
+   // const res=await library.insertOne({});
+   //let jsonDocument={message:"Hello world!",to:"Ashitosh" ,from:"Atish"};
+
+   await library.insertOne(jsonDocument);
+
+    //close the connection
+    await client.close();
+
+    console.log("Record Added");
+
+}
+
+async function read
