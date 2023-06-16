92ad5bd776eefe92a0d75e1526335ffa85b4b8a3
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-16 10:23:01 +0530

    helloworld-in-express

--- src/main.js
+++ src/main.js
@@ -7,5 +7,13 @@
 import  express  from "express";
 const app = express()
 
+function main(req,res){
+  //  return "hello";
+  res.send("some message....!!")
+}
 
+// http://localhost:4000/main
+app.get("/main",main);
+
+// http://localhost:4000
 app.listen(4000);
