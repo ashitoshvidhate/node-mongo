c25a53f861a20490a2392781377c0f84584c10de
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-06-16 11:11:44 +0530

    insert-read-node-mongo-express-node

--- src/main.js
+++ src/main.js
@@ -1,19 +1,38 @@
+import { MongoClient } from "mongodb";
+import express from "express";
+const app = express();
 
-//remotely executable
-//NetWork call
-//Network address
-//Remote Address
-//http://localhost:4000/
-import  express  from "express";
-const app = express()
+async function main(req, res) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
 
-function main(req,res){
-  //  return "hello";
-  res.send("some message....!!")
+  const db = client.db("mydb");
+  const messageColl = db.collection("library");
+
+  //let message = req.query.message;
+  let inputDocument = { message: "hello Mongo Node Express" };
+  await messageColl.insertOne(inputDocument);
+
+  await client.close();
+
+  res.send("record added");
 }
 
-// http://localhost:4000/main
-app.get("/main",main);
+async function findAllMessage(req, res) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
 
-// http://localhost:4000
+  const db = client.db("mydb");
+  const messageColl = db.collection("library");
+
+  let list = await messageColl.find().toArray();
+
+  await client.close();
+  res.send(list);
+}
+
+app.get("/main", main);
+app.get("/find", findAllMessage);
+
+// http://loaclhost:4000
 app.listen(4000);
